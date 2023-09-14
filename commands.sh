78604c4cbb22b96fc74102b28f3f98db3baaf9f0
César Vega <58005712+cetusarto@users.noreply.github.com>
2023-09-13 20:11:40 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-sudo apt install git-all
-sudo apt update
-sudo apt install python3 
 PROJECT_ID = $(gcloud config get project)
 gcloud config set project $PROJECT_ID
 pip install requests

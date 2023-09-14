42d38872b0da6588c413dede6fc4709db6966824
cesar augusto vega diaz <colombianoxd4@gmail.com>
2023-09-13 19:55:41 -0500

    pushing

---
+++ commands.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+sudo apt update
+sudo apt install python3 
+PROJECT_ID = $(gcloud config get project)
+gcloud config set project $PROJECT_ID
+pip install requests
+pip install pandas
+pip install google-cloud-bigquery
+pip install pandas-gbq -U
+pip install sqlite3
